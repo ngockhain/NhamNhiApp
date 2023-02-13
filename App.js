1d58ab922c23cb6b042d86dbe726fee3a4dda048
nn-khai <ngockhain@gmail.com>
2023-02-14 00:05:30 +0700

    drawer nagivation

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from "@react-navigation/native";
 import {
   Menu, Avatar, HStack, HamburgerIcon, Icon, VStack,
-  IconButton, Pressable, Text, Center, Box, StatusBar
+  IconButton, Pressable, Text, Center, Box, StatusBar,
+  Divider
 } from "native-base";
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 import {
   createDrawerNavigator,
@@ -13,13 +17,126 @@ import {
 
 const Drawer = createDrawerNavigator();
 
-function CustomDrawerContent(props) {
+const getIcon = (screenName) => {
+  switch (screenName) {
+    case "Thống kê":
+      return "chart-bar-stacked";
+    case "Thêm dữ liệu":
+    case "Thêm loại":
+      return "database-plus-outline";
+    case "Favorites":
+      return "heart";
+    case "Archive":
+      return "archive";
+    case "Trash":
+      return "trash-can";
+    case "Spam":
+      return "alert-circle";
+    default:
+      return undefined;
+  }
+};
+
+export function CustomDrawerContent(props) {
+
   return (
     <DrawerContentScrollView {...props} safeArea>
       <VStack space="6" my="2" mx="1">
-        <Text>Thống kê</Text>
-        <Text>Thêm dữ liệu</Text>
-        <Text>Thêm dữ liệu 1</Text>
+        <Box px="4">
+          <HStack>
+            <Avatar bg="amber.500" source={{
+              uri: "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            }}></Avatar>
+            <VStack marginLeft={5}>
+              <Text fontSize="14" mt="1" fontWeight="500">
+                Vịt cạp cạp
+              </Text>
+              <Text fontSize="12" color="gray.500" >
+                Vịt hăm ăn chấu chí
+              </Text>
+            </VStack>
+          </HStack>
+        </Box>
+        <VStack divider={<Divider />} space="4">
+          <VStack space="3">
+            {props.state.routeNames.map((name, index) => (
+              <Pressable
+                px="5"
+                py="3"
+                rounded="md"
+                bg={
+                  index === props.state.index
+                    ? "rgba(6, 182, 212, 0.1)"
+                    : "transparent"
+                }
+                onPress={(event) => {
+                  props.navigation.navigate(name);
+                }}
+              >
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.500"
+                    }
+                    size="5"
+                    as={<MaterialCommunityIcons name={getIcon(name)} />}
+                  />
+                  <Text
+                    fontWeight="500"
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.700"
+                    }
+                  >
+                    {name}
+                  </Text>
+                </HStack>
+              </Pressable>
+            ))}
+          </VStack>
+          <VStack space="5">
+            <Text fontWeight="500" fontSize="14" px="5" color="gray.500">
+              Labels
+            </Text>
+            <VStack space="3">
+              <Pressable px="5" py="3">
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color="gray.500"
+                    size="5"
+                    as={<MaterialCommunityIcons name="bookmark" />}
+                  />
+                  <Text color="gray.700" fontWeight="500">
+                    Family
+                  </Text>
+                </HStack>
+              </Pressable>
+              <Pressable px="5" py="2">
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color="gray.500"
+                    size="5"
+                    as={<MaterialCommunityIcons name="bookmark" />}
+                  />
+                  <Text color="gray.700" fontWeight="500">
+                    Friends
+                  </Text>
+                </HStack>
+              </Pressable>
+              <Pressable px="5" py="3">
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color="gray.500"
+                    size="5"
+                    as={<MaterialCommunityIcons name="bookmark" />}
+                  />
+                  <Text fontWeight="500" color="gray.700">
+                    Work
+                  </Text>
+                </HStack>
+              </Pressable>
+            </VStack>
+          </VStack>
+        </VStack>
       </VStack>
     </DrawerContentScrollView>
   )
@@ -40,16 +157,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-        <Box safeArea flex={1}>
+        <Box flex={1}>
           <Drawer.Navigator
             drawerContent={(props) => <CustomDrawerContent {...props} />}
           >
-            <Drawer.Screen name="Inbox" component={Component} />
-            <Drawer.Screen name="Outbox" component={Component} />
-            <Drawer.Screen name="Favorites" component={Component} />
-            <Drawer.Screen name="Archive" component={Component} />
-            <Drawer.Screen name="Trash" component={Component} />
-            <Drawer.Screen name="Spam" component={Component} />
+            <Drawer.Screen name="Thống kê" component={Component} />
+            <Drawer.Screen name="Thêm dữ liệu" component={Component} />
+            <Drawer.Screen name="Thêm loại" component={Component} />
           </Drawer.Navigator>
         </Box>
       </NativeBaseProvider>
